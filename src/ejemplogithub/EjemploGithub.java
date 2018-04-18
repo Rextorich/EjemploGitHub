9fcf87ec7c5121822709a738c84409a10194b008
Rextorich <33941963+Rextorich@users.noreply.github.com>
2018-04-18 00:09:18 -0500

    Update EjemploGithub.java

--- src/ejemplogithub/EjemploGithub.java
+++ src/ejemplogithub/EjemploGithub.java
@@ -17,6 +17,7 @@ public class EjemploGithub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Richard");
+        System.out.println("Hola");
     }
     
 }
